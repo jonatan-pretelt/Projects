90e9b2e570b36678224585a342d53a9289cf6296
jonatan <jonatan.pretelt@gmail.com>
2025-12-04 22:20:04 -0500

    learning java

---
+++ LearningJava/day01/Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+
+public class Main{
+    public static void main(String[] args) {
+       LocalDateTime now = LocalDateTime.now();
+       
+       System.out.println("Hi, you code today at: " + now);
+    }
+}
